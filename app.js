0c47ddd7f0b737d4ecaae4cc0f2157e55aac1c2c
MuktaHD <muktabhosale3@gmail.com>
2024-05-28 15:32:06 +0530

    product get post and update delete

---
+++ app.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const bodyParser=require('body-parser');
+const productRoutes=require('./routes/productRoute');
+//userRoute added
+const userRoutes=require('./routes/userRoute');
+require('dotenv').config();
+
+const app=express();
+const port=process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.once('open',()=>{
+    console.log('connected to MongoDB database');
+});
+
+app.use('/api',productRoutes);
+
+//userRoute added
+
+app.use('/api/users',userRoutes);
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+});
---
+++ routes/productRoute.js
@@ -0,0 +1,59 @@
+const express=require('express');
+const router=express.Router();
+const Product=require('../models/productModel');
+
+const app=express();
+
+router.post('/products',async(req,res)=>{
+    const product=new Product(req.body);
+    try{
+        await product.save();
+        res.status(201).send(product);
+    }catch (error){
+        res.status(400).send(error);
+    }
+});
+
+
+router.get('/products',async(req,res)=>{
+    try{
+    const products=await Product.find();
+    res.status(200).send(products);
+    }catch (error){
+        res.status(500).send(error);
+    }
+});
+
+
+
+router.get('/product:id',async(req,res)=>{
+    const _id=req.params.id;
+    const product=await Product.findById(_id);
+    if(!product){
+        return res.status(404).send();
+    }
+    res.send(product);
+});
+
+router.patch('/productUpdate:id',async(req,res)=>{
+    const _id=req.params.id;
+    const product=await Product.findByIdAndUpdate(_id,req.body,{new:true});
+    if(!product){
+        return res.status(404).send();
+    }
+    res.send(product);
+})
+
+
+router.delete('/productDelete:id',async(req,res)=>{
+    const _id=req.params.id;
+    const product=await Product.findByIdAndDelete(_id);
+    if(!product){
+        return res.status(404).send();
+    }
+    res.send(product);
+});
+
+
+
+module.exports=router;
