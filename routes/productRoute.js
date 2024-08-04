d4567eb226a7387bf74029d6bff6df8ba6390622
MuktaHD <165270785+MuktaHD@users.noreply.github.com>
2024-08-04 21:34:57 +0530

    Update productRoute.js

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,7 +1,6 @@
 const express=require('express');
 const router=express.Router();
 const Product=require('../models/productModel');
-
 const app=express();
 
 router.post('/products',async(req,res)=>{
